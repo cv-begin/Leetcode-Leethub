5bf3b828fbc40beeede9055b4c757deee912fa26
cv-begin <107246599+cv-begin@users.noreply.github.com>
2023-07-17 23:15:45 +0900

    Time: 420 ms (92.46%), Space: 31 MB (62.16%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,12 +3,19 @@ class Solution:
         # modified neetcode solution
         
         longest = 0
+        
         numSet = set(nums)
         
         for num in numSet:
+            
+            length = 1
             if (num-1) not in numSet:
-                length = 1
                 while (num+length) in numSet:
                     length += 1
-                longest = max(longest, length)
+                longest = max(length, longest)
         return longest
+    
+    
+    
+                    
+            
